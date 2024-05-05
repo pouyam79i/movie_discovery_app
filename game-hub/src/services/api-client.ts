e0937c13846e1e900b0f045777759ae574654492
Pouya <pouyamohammadyirbu@gmail.com>
2024-05-05 10:42:03 +0330

    update

--- game-hub/src/services/api-client.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://rawg-video-games-database.p.rapidapi.com",
-  headers: {
-    "X-RapidAPI-Key": "",
-    "X-RapidAPI-Host": "",
-  },
-});
