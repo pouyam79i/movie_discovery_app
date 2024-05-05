c02ba5bb67b7fc2bc55cc5b541cb26393efb3bb6
Pouya <pouyamohammadyirbu@gmail.com>
2024-05-05 17:12:58 +0330

    fetching movies with api call

--- game-hub/src/components/MoviesGrid.tsx
+++ game-hub/src/components/MoviesGrid.tsx
@@ -1,31 +1,8 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
 import { Text } from "@chakra-ui/react";
-
-interface Movie {
-  id: number;
-  title: string;
-  original_title: string;
-  overview: string;
-  popularity: number;
-  // poster_path: string;
-}
+import useMovies from "../hooks/useMovies";
 
 const MovieGrid = () => {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    console.log("Trying");
-    apiClient
-      .get<Movie[]>("/Random/20")
-      .then((res) => {
-        console.log("I got res");
-        console.log(res.data);
-        setMovies(res.data);
-      })
-      .catch((err) => setError(err.message));
-  }, []);
+  const { movies, error } = useMovies();
 
   return (
     <>
---
+++ game-hub/src/hooks/useMovies.ts
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface Movie {
+  id: number;
+  title: string;
+  //   original_title: string;
+  //   overview: string;
+  release_date: string;
+  vote_average: number;
+  //   adult: boolean;
+  poster_path: string;
+}
+
+interface FetchedMovies {
+  results: Movie[];
+}
+
+const useMovies = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    console.log("Trying");
+    apiClient
+      .get<FetchedMovies>("", { signal: controller.signal })
+      .then((res) => {
+        console.log("I got res");
+        console.log(res.data.results);
+        setMovies(res.data.results);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { movies, error };
+};
+
+export default useMovies;
