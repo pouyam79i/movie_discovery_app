2dcae69b0021b7727c2b707a905e042a25c473ff
Pouya <pouyamohammadyirbu@gmail.com>
2024-05-19 12:16:06 +0330

    fix: image error when not loaded

--- game-hub/src/components/MovieCard.tsx
+++ game-hub/src/components/MovieCard.tsx
@@ -1,19 +1,26 @@
 import { Card, CardBody, Heading, Image, VStack } from "@chakra-ui/react";
-
+import { ReactNode, useRef } from "react";
 import MovieDescription from "./MovieDescription";
 import { Movie } from "../hooks/useMovies";
+import emptyCover from "../assets/emptycover.webp";
 
 interface Props {
   movie: Movie;
 }
 
 const MovieCard = ({ movie }: Props) => {
+  const imgRef = useRef<any>();
+  const onImageError = () => (imgRef.current.src = emptyCover);
+
   return (
     <Card>
       <Image
+        ref={imgRef}
         src={
           "https://image.tmdb.org/t/p/w600_and_h900_bestv2" + movie.poster_path
         }
+        onError={onImageError}
+        alt={emptyCover}
       />
       <CardBody
         flexDirection={"column"}
