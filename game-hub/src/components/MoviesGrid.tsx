dcac5d996518c44a2627fc813b335925d649b07b
Pouya <pouyamohammadyirbu@gmail.com>
2024-05-19 18:21:41 +0330

    update: dark mode color is customized

--- game-hub/src/components/MoviesGrid.tsx
+++ game-hub/src/components/MoviesGrid.tsx
@@ -18,9 +18,9 @@ const MovieGrid = ({ movieQuery }: Props) => {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding={5}
-        spacing={3}
+        spacing={6}
       >
         {isLoading &&
           skeletons.map((item) => (
